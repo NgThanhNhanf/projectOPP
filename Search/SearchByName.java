9ec8717973b4b0d4e47e7d56a4e0827b9dbca37d
Thanh Hai <thanhhai1512005@gmail.com>
2024-11-08 17:36:20 +0700

    1, 2, 5 search

--- Search/SearchByName.java
+++ Search/SearchByName.java
@@ -1,5 +1,28 @@
 package Search;
 
+import java.util.Scanner;
+
+import Model.Inventory;
+import Model.Product;
+
 public class SearchByName implements SearchMethod {
-    
+    Scanner sc = new Scanner(System.in);
+    public void search(){
+        // sc.nextLine();
+        boolean ktName = false;
+        do{
+            System.out.print("Nhap ten san pham can tim : ");
+            String isProductName = sc.nextLine();
+            Product resProductName = Inventory.getProductByName(isProductName);
+            if(resProductName != null){
+                ktName = true;
+                Inventory.displayProductByName(isProductName);
+            }else{
+                System.out.println("Ten san pham khong ton tai. Ban co muon nhap lai ? (y / n)");
+                
+                String chooseName = sc.nextLine();
+                ktName = !chooseName.equalsIgnoreCase("y");
+            }
+        }while(!ktName);
+    }
 }
