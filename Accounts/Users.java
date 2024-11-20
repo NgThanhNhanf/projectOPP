0f584303419854cb8725d1e5f32f2f9f3811c406
Nguyen Hoang Anh <nha261105@gmail.com>
2024-11-20 18:47:57 +0700

    fix bug

--- Accounts/Users.java
+++ Accounts/Users.java
@@ -15,9 +15,9 @@ public class Users implements fileWork, SearchList{
     @Override
     public void readFile() throws FileNotFoundException {
         // File myFile = new File("D:\\Study\\OOP\\projectOPP\\Accounts\\accountsData.txt");
-        // File myFile = new File("D:\\Java\\Nhom14\\OOP-hanh\\DoAnOOP\\Project\\Accounts\\accountsData.txt");
+        File myFile = new File("D:\\Java\\Nhom14\\OOP-hanh\\DoAnOOP\\Project\\Accounts\\accountsData.txt");
         // File myFile = new File("D:\\Workspace\\Test\\temp\\projectOPP\\Accounts\\accountsData.txt");
-        File myFile = new File("C:\\OOP\\projectOPP\\Accounts\\accountsData.txt");
+        // File myFile = new File("C:\\OOP\\projectOPP\\Accounts\\accountsData.txt");
         Scanner scf = new Scanner(myFile);
         while (scf.hasNextLine()) {
             String line = scf.nextLine();
@@ -30,9 +30,9 @@ public class Users implements fileWork, SearchList{
     @Override
     public void writeFile() throws IOException {
         // FileWriter myFile = new FileWriter("D:\\Study\\OOP\\projectOPP\\Accounts\\accountsData.txt");
-        // FileWriter myFile = new FileWriter("D:\\Java\\Nhom14\\OOP-hanh\\DoAnOOP\\Project\\Accounts\\accountsData.txt");
+        FileWriter myFile = new FileWriter("D:\\Java\\Nhom14\\OOP-hanh\\DoAnOOP\\Project\\Accounts\\accountsData.txt");
         // FileWriter myFile = new FileWriter("D:\\Workspace\\Test\\temp\\projectOPP\\Accounts\\accountsData.txt");
-        FileWriter myFile = new FileWriter("C:\\OOP\\projectOPP\\Accounts\\accountsData.txt");
+        // FileWriter myFile = new FileWriter("C:\\OOP\\projectOPP\\Accounts\\accountsData.txt");
         for (User cur : users) {
             myFile.write(cur.getUserName() + '|' + cur.getPassword() + '|' + cur.getUserId() + '|' + cur.getRole() + '\n');
         }
--- Statistic/Store.java
+++ Statistic/Store.java
@@ -13,13 +13,13 @@ import java.util.Scanner;
 
 public class Store {
     private static  LocalDate day;
-    private static HashMap<Orders,Integer> statistic = new HashMap<>();
+    private static HashMap<Integer,List<Order>> listOnQuarter = new HashMap<>();
     private static List<Order> listOnDay = new ArrayList<>();
     private static List<Order> listOnMonth = new ArrayList<>();
     private static List<Order> listOnYear = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
 
-    static DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    static DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     static SimpleDateFormat sformat = new SimpleDateFormat("MM-yyyy");
 
 
@@ -160,6 +160,30 @@ public class Store {
     }
     
 
+    // public static void statisticInQuarter() {
+    //     System.out.println("nhap quy: ");
+    //     int quarter = sc.nextInt();
+    //     boolean checkQuarter = false;
+    //     switch (quarter) {
+    //         case 1,2,3,4:
+    //             checkQuarter = true;                
+    //             break;
+    //         default:
+    //             checkQuarter = false;
+    //             break;
+    //     }
+    //     listOnQuarter.put(checkQuarter, new ArrayList<>());
+    //     for(Order order : listOnMonth) {
+    //         String date = order.getOrderDate().toString();
+    //         String[] str = date.split("-");
+    //         int monthInDate = Integer.parseInt(str[1]);
+    //         if(monthInDate == 1 || monthInDate == 2 || monthInDate == 3) {
+               
+    //         }
+    //     }
+
+    // }
+
     public static void solve() {
         boolean isStatistic = false;
         while(!isStatistic) {
@@ -167,7 +191,8 @@ public class Store {
             System.out.println("1.Thong ke theo ngay");
             System.out.println("2.Thong ke theo thang");
             System.out.println("3.thong ke theo nam");
-            System.out.println("4.thoat");
+            System.out.println("4.thong ke theo quy");
+            System.out.println("5.thoat");
             int choice;
             System.out.print("nhap lua chon: ");
             do {
@@ -194,6 +219,8 @@ public class Store {
                     statisticInYear();
                     break;
                 case 4:
+                    // statisticInQuarter();
+                case 5:
                     System.out.println("thoat");
                     isStatistic = true;
                     break;
